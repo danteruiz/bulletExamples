c7f4e7a9f55047dd037b3a86df3deed0583e28a8
danteruiz <danteruiz102@gmail.com>
2020-09-15 11:44:44 -0700

    fixing pbr shader

--- resources/shaders/simple.fs
+++ resources/shaders/simple.fs
@@ -41,37 +41,36 @@ in vec3 vNormal;
 in vec3 vPosition;
 in vec2 TexCoord;
 const float PI = 3.14159265359;
-float NDF(float cosTheta, float roughness)
+
+float NDF(float NdotH, float roughness)
 {
-    float roughness2 = roughness * roughness;
-    float cosTheta2 = cosTheta * cosTheta;
-    float denom = (cosTheta2 * ( roughness2 - 1.0) + 1.0);
-    return cosTheta2 / (max(PI * ( denom * denom), 0.001));
+    float alpha = roughness * roughness;
+    float roughness2 = alpha * alpha;
+    float f = (NdotH * roughness2 - NdotH) * NdotH + 1.0;
+    return roughness2 / (PI * f * f);
 }
-vec3  Fensel(float cosTheta, vec3 f)
+vec3 Fensel(float VdotH, vec3 f)
 {
-    return f + (1.0 -f) * pow((1.0 - cosTheta), 5.0);
+    return f + (1.0 -f) * pow((1.0 - VdotH), 5.0);
 }
 
 float ShlickGGX(float NdotV, float roughness)
 {
-    float r = (roughness + 1.0);
+    float r = roughness;
     float k = (r*r) / 8.0;
     return (NdotV / ( NdotV * (1.0 - k) + k));
 }
 
-float GSmith(vec3 n, vec3 v, vec3 l, float roughness)
+float GSmith(float NdotL, float NdotV, float roughness)
 {
-    float NdotL = max(dot(n, l), 0.0);
-    float NdotV = max(dot(n, v), 0.0);
+    float r = roughness * roughness;
 
+    float attenuationL = 2.0 * NdotL / (NdotL + sqrt(r * r + (1.0 - r * r) * (NdotL * NdotL)));
+	float attenuationV = 2.0 * NdotV / (NdotV + sqrt(r * r + (1.0 - r * r) * (NdotV * NdotV)));
+	//return attenuationL * attenuationV;
     return (ShlickGGX(NdotL, roughness) * ShlickGGX(NdotV, roughness));
 
 }
-
-
-
-
 
 void main() {
     vec3 v = normalize(cameraPosition - vPosition);
@@ -79,39 +78,51 @@ void main() {
     vec3 h = normalize(v + l);
     vec3 n = vNormal;
 
-    vec3 diffuseColor = texture(colorMap, TexCoord);
+    vec3 diffuseColor;
+    vec3 baseColor = texture(albedoMap, TexCoord).rgb * material.color;
+    float perceptualRoughness = material.roughness;
+    float metallic = material.metallic;
+    vec3 f0 = vec3(0.04);
 
-    float cosTh = max(dot(vNormal, h), 0.0);
-    float cosTi = max(dot(vNormal, l), 0.0);
+    vec4 mrSample = texture(metallicMap, TexCoord);
+
+    perceptualRoughness = mrSample.g * perceptualRoughness;
+    metallic = mrSample.b * metallic;
+
+    diffuseColor = baseColor * (vec3(1.0 - f0));
+    diffuseColor *= 1.0 - metallic;
+
+    float alphaRoughness = perceptualRoughness * perceptualRoughness;
+
+    vec3 specularColor = mix(f0, diffuseColor, metallic);
+
+    float cosTh = max(dot(n, h), 0.0);
+    float cosTi = max(dot(n, l), 0.0);
+
+
+    float NdotL = clamp(dot(n, l), 0.001, 1.0);
+    float NdotV = clamp(abs(dot(n, v)), 0.001, 1.0);
+    float NdotH = clamp(dot(n, h), 0.0, 1.0);
+    float LdotH = clamp(dot(l, h), 0.0, 1.0);
+    float VdotH = clamp(dot(v, h), 0.0, 1.0);
 
     float distance = length(light.position - vPosition);
     float attenuation = 1.0;
-    
+
     vec3 radiance = (light.color)  * attenuation;
-    vec3 fo = vec3(0.04);
-    fo = mix(fo, material.color.rgb, material.metallic);
 
-    
-    float ndf = NDF(cosTh, material.roughness);
-    float g = GSmith(n, v, l, material.roughness);
-    vec3 fr = Fensel(clamp(dot(h,v), 0.0, 1.0), fo);
-
-    vec3 numerator = ndf * g * fr;
-    float denominator = 4.0 * max(dot(n, v), 0.0) * max(dot(n,l), 0.0);
-    vec3 specular = numerator / max(denominator, 0.0001);
+    float ndf = NDF(cosTh, perceptualRoughness);
+    float g = GSmith(NdotL, NdotV, perceptualRoughness);
+    vec3 fr = Fensel(VdotH, specularColor);
 
 
-    vec3 ks = fr;
-    vec3 kd = vec3(1.0) - ks;
-    kd *= 1.0 - material.metallic;
+    vec3 diffuseContrib = (1.0 - fr) * (diffuseColor / PI);
+    vec3 specContrib = ndf * g * fr / (4.0 * NdotL * NdotV);
 
-
-    vec3 colorr = texture(albedoMap, TexCoord).rgb *  material.color.rgb;
-    vec3 lo = (kd * colorr / PI + specular) * radiance * cosTi;
-    vec3 ambient = colorr * light.ambient * material.ao;
-    vec3 color = ambient + lo;
+    vec3 color = NdotL * light.color * (diffuseContrib + specContrib);
 
     //color = color / (color + vec3(1.0));
     //color = color = pow(color, vec3(1.0 / 2.2));
+    //color = baseColor;
     FragColor = vec4(color, 1.0);
 }
